532e3d6f1f7d7f20644f9fd7599e1ebe15956408
kevinxyang <kyang@fullsail.edu>
2014-04-01 15:36:40 -0400

    Fixed console.log and output.

--- Week 1/goal1_assign_duel1/js/main.js
+++ Week 1/goal1_assign_duel1/js/main.js
@@ -8,9 +8,9 @@
 // Givens
 // ------
 
-var playerOneName = "Kevin";
-var playerTwoName = "Jason";
-var playerOneHealth = playerTwoHealth = 1000;
+var playerOneName = prompt("What is player 1's name?", "Spiderman");
+var playerTwoName = prompt("What is player 2's name?", "Batman");
+var playerOneHealth = playerTwoHealth = 100;
 var playerOneMaxDamage = playerTwoMaxDamage = 50;
 var round = 1;
 
@@ -26,56 +26,60 @@ function fight(){
 		var result = "";
 		if (round == 10){
 			if (playerOneHealth == playerTwoHealth){
-				result = "It's a draw!"
-				console.log("Draw!");
+				result = "It's a Draw!"
+				console.log("Result: It's a Draw");
 			}
 			else if (playerOneHealth > playerTwoHealth){
-				result = "Player 1 is victorious!"
-				console.log("The winner is player 1.");
+				result = "Player 1 Wins"
+				console.log("Result: Player 1 Wins");
 			}
 			else {
-				result = "Player 2 is victorious!"
-				console.log("The winner is player 2.");
+				result = "Player 2 Wins"
+				console.log("Result: Player 2 Wins");
 			}
 		}
 		else if (playerOneHealth <= 0 && playerTwoHealth <= 0){
-			result = "Both players have fallen, there is no winner."
-			console.log("No winner.");
+			result = "You Both Die"
+			console.log("Result: You Both Die");
 		}
 		else if (playerOneHealth <= 0){
-			result = "Player 2 is victorious!"
-			console.log("The winner is player 2.");
+			result = "Player 2 Wins"
+			console.log("Result: Player 2 Wins");
 		}
 		else if (playerTwoHealth <= 0){
-			result = "Player 1 is victorious!"
-			console.log("The winner is player 1.");
+			result = "Player 1 Wins"
+			console.log("Result: Player 1 Wins");
 		}
 		else {
-			result = "The battle rages on!"
-			console.log("No winner yet.");
+			result = "The Battle Rages On!"
+			console.log("Result: No Winner");
 		}
 
 		return result;
 	}
 
-	var currentResult = "Let the fight commence!";
-	console.log(round);
-	console.log(playerOneHealth);
-	console.log(playerTwoHealth);
-	alert("Round " + round + " Player One HP: " + playerOneHealth + " Player Two HP: " + playerTwoHealth + " " + currentResult);
-	for (var i = 9; i > 0; i--){
+	var currentResult = "*START*";
+	console.log("Round " + round);
+	console.log(playerOneName + ":" + playerOneHealth + " " + playerTwoName + ":" + playerTwoHealth);
+	console.log("Fight!");
+	console.log("--------------");
+	alert(playerOneName + ":" + playerOneHealth + " " + currentResult + " " + playerTwoName + ":" + playerTwoHealth);
+	for (var i = 10; i > 0; i--){
 		round++;
 		playerOneDamage = Math.floor(Math.random() * 25 + 25);
 		playerTwoDamage = Math.floor(Math.random() * 25 + 25);
 		playerOneHealth -= playerTwoDamage;
 		playerTwoHealth -= playerOneDamage;
-		console.log(round);
-		console.log(playerOneHealth);
-		console.log(playerTwoHealth);
+		console.log("Round " + round);
+		console.log(playerOneName + ":" + playerOneHealth + " " + playerTwoName + ":" + playerTwoHealth);
 		currentResult = winnerCheck();
-		alert("Round " + round + " Player One HP: " + playerOneHealth + " Player Two HP: " + playerTwoHealth + " " + currentResult);
-		if (playerOneHealth <= 0 || playerTwoHealth <= 0) {
+		console.log("--------------");
+		if (playerOneHealth <= 0 || playerTwoHealth <= 0 || round == 10) {
+			alert(currentResult);
 			break;
+		}
+		else {
+			alert(playerOneName + ":" + playerOneHealth + " *ROUND " + round + " OVER* " + playerTwoName + ":" + playerTwoHealth);
 		}
 	}
 }
