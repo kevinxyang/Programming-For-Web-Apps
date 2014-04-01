6544ad09017758c8666ae6136f9f87cdf4cd4a3d
kevinxyang <kyang@fullsail.edu>
2014-04-01 12:53:11 -0400

    Wrote fight() and winnerCheck().

--- Week 1/goal1_assign_duel1/js/main.js
+++ Week 1/goal1_assign_duel1/js/main.js
@@ -1,1 +1,67 @@
-console.log("Hello World!");
+// Kevin Yang
+// April 1st, 2014
+// The Duel - Part I
+// =================
+
+
+
+// Givens
+// ------
+
+var playerOneName = "Kevin";
+var playerOneHealth = 100;
+var playerOneMaxDamage = 50;
+
+var playerTwoName = "Jason";
+var playerTwoHealth = 100;
+var playerTwoMaxDamage = 50;
+
+
+
+// Functions
+// ---------
+
+function fight(){
+
+	function winnerCheck(){
+		if (playerOneHealth <= 0 && playerTwoHealth <= 0){
+			console.log("No winner.");
+		}
+		else if (playerOneHealth <= 0){
+			console.log("The winner is player 2.");
+		}
+		else if (playerTwoHealth <= 0){
+			console.log("The winner is player 1.");
+		}
+		else {
+			console.log("No winner yet.");
+		}
+	}
+
+	var round = 1;
+	console.log(round);
+	console.log(playerOneHealth);
+	console.log(playerTwoHealth);
+	for (var i = 9; i > 0; i--){
+		round++;
+		playerOneDamage = Math.floor(Math.random() * 25 + 25);
+		playerTwoDamage = Math.floor(Math.random() * 25 + 25);
+		playerOneHealth -= playerTwoDamage;
+		playerTwoHealth -= playerOneDamage;
+		console.log(round);
+		console.log(playerOneHealth);
+		console.log(playerTwoHealth);
+		winnerCheck();
+		if (playerOneHealth <= 0 || playerTwoHealth <= 0) {
+			break;
+		}
+
+	}
+}
+
+
+
+// Output
+// ------
+
+fight();
