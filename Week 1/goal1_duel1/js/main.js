8a907b7d608eddd865182f3dd1d84234bb6a2288
kevinxyang <kyang@fullsail.edu>
2014-04-02 11:32:02 -0400

    Wrote the fight function.

--- Week 1/goal1_duel1/js/main.js
+++ Week 1/goal1_duel1/js/main.js
@@ -5,8 +5,33 @@
 
 
 
-function fight(nameOne, nameTwo, damageOne, damageTwo, healthOne, healthTwo) {
-	console.log("Hello World");
+function checkWinner(name1, name2, health1, health2) {
+	console.log("Hello World!");
+} 
+
+function fight(name1, name2, damage1, damage2, health1, health2) {
+    'use strict';
+    var round = 1;
+    var playerOneMinDamage = damage1 * .5;
+    var playerTwoMinDamage = damage2 * .5;
+    for (var i = 0; i < 10; i++) {
+        var playerOneDamage = Math.floor(Math.random() * (damage1 - playerOneMinDamage) + playerOneMinDamage);
+        var playerTwoDamage = Math.floor(Math.random() * (damage2 - playerTwoMinDamage) + playerTwoMinDamage);
+        health1 -= playerOneDamage;
+        health2 -= playerTwoDamage;
+        round++;
+        alert(name1 + ":" + health1 + " *ROUND " + round + "* " + name2 + ":" + health2);
+        console.log("---- Round " + round + " ----");
+        console.log(name1 + ":" + health1, name2 + ":" + health2);
+        var currentResult = winnerCheck(name1, name2, health1, health2, round);
+        if (currentResult === "No Winner") {
+            console.log(currentResult);
+        } else {
+            alert(currentResult);
+            console.log(currentResult);
+            break;
+        }
+    }
 }
 
 function init() {
