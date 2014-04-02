3ca699a2cf8d34bdbe953915f5e485ffdc2e9d2a
kevinxyang <kyang@fullsail.edu>
2014-04-02 11:49:17 -0400

    Added comments for the fight() function.

--- Week 1/goal1_duel1/js/main.js
+++ Week 1/goal1_duel1/js/main.js
@@ -28,21 +28,31 @@ function winnerCheck(name1, name2, health1, health2, currentRound) {
     return result;
 } // End of winnerCheck() function.
 
+// Takes our givens as arguments.
 function fight(name1, name2, damage1, damage2, health1, health2) {
     'use strict';
+    // Sets the round to 1.
     var round = 1;
+    // Minimum player damage.
     var playerOneMinDamage = damage1 * .5;
     var playerTwoMinDamage = damage2 * .5;
+    /* For each round recalculate the damage for each player, and subtract it from the other player's 
+       remaining health. */
     for (var i = 0; i < 10; i++) {
         var playerOneDamage = Math.floor(Math.random() * (damage1 - playerOneMinDamage) + playerOneMinDamage);
         var playerTwoDamage = Math.floor(Math.random() * (damage2 - playerTwoMinDamage) + playerTwoMinDamage);
         health1 -= playerOneDamage;
         health2 -= playerTwoDamage;
+        // Increment the round by 1.
         round++;
+        // Alert the user about the updated information.
         alert(name1 + ":" + health1 + " *ROUND " + round + "* " + name2 + ":" + health2);
+        // Print the results to the console.
         console.log("---- Round " + round + " ----");
         console.log(name1 + ":" + health1, name2 + ":" + health2);
+        // Call the winnerCheck() function, and store the result in currentResult.
         var currentResult = winnerCheck(name1, name2, health1, health2, round);
+        // If winnerCheck() returns a result other than "No Winner", exit the loop.
         if (currentResult === "No Winner") {
             console.log(currentResult);
         } else {
