4f558ad0a90867dcb37e1055d167723f98df31b1
kevinxyang <kyang@fullsail.edu>
2014-04-08 19:21:01 -0400

    Wrote the init() function, and called it after the window has loaded.

--- Week 2/goal1_assign_duel2/js/main.js
+++ Week 2/goal1_assign_duel2/js/main.js
@@ -2,3 +2,22 @@
 // April 8th, 2014
 // The Duel - Part II
 // ==================
+
+
+
+// The function is called after the window has loaded.
+function init() {
+    'use strict';
+    // Stores the player names, damage, and health in an array.
+    var playerOne = ["Spiderman", 20, 100];
+    var playerTwo = ["Batman", 20, 100];
+    // Alerts the user about the starting conditions.
+    alert(playerOne[0] + ":" + playerOne[2] + " *START* " + playerTwo[0] + ":" + playerTwo[2]);
+    console.log("Begin!");
+    // Calls the fight() function.
+    fight(playerOne, playerTwo);
+} // End of the init() function.
+
+
+
+window.onload = init;
