9f46c9a95014d927b501308337926558424ccd39
kevinxyang <kyang@fullsail.edu>
2014-04-15 14:32:09 -0400

    Wrote the fight() function.

--- Week 3/goal4_assign_duel3/js/main.js
+++ Week 3/goal4_assign_duel3/js/main.js
@@ -5,6 +5,36 @@
 
 
 
+// The fight function is called after the button has been clicked.
+function fight(myArray, currentRound) {
+    'use strict';
+    // Calculates each player's minimum damage.
+    var playerOneMinDamage = myArray[0].damage * .5;
+    var playerTwoMinDamage = myArray[1].damage * .5;
+    // Damage Formula.
+    var playerOneDamage = Math.floor(Math.random() * (myArray[0].damage - playerOneMinDamage) + playerOneMinDamage);
+    var playerTwoDamage = Math.floor(Math.random() * (myArray[1].damage - playerTwoMinDamage) + playerTwoMinDamage);
+    // Subtracts each player's damage from the opposing player's health.
+    myArray[0].health -= playerOneDamage;
+    myArray[1].health -= playerTwoDamage;
+    // Find the element in the document with ID equal to round and display the current round within the HTML.
+    document.getElementById("round").innerHTML = "ROUND " + currentRound + " Complete";
+    document.getElementsByTagName("p")[0].innerHTML = myArray[0].name + ": " + myArray[0].health;
+    document.getElementsByTagName("p")[1].innerHTML = myArray[1].name + ": " + myArray[1].health;
+    console.log("---- Round " + currentRound + " ----");
+    console.log(myArray[0].name + ":" + myArray[0].health, myArray[1].name + ":" + myArray[1].health);
+    var currentResult = winnerCheck(myArray, currentRound);
+    if (currentResult !== "No Winner") {
+        document.getElementsByTagName("p")[0].innerHTML = currentResult;
+        document.getElementsByTagName("p")[1].innerHTML = currentResult;
+        document.getElementsByTagName("a")[0].onclick = " ";
+    }
+    console.log(currentResult);
+
+} // End of the fight() function.
+
+
+
 // The function is called after the window has loaded.
 function init() {
     'use strict';
